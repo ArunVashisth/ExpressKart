dabc9bf5b1c49d09d3925c32f9efef4dc94aed8f
Arun Vashisth <arunvashisth80@GMAIL.COM>
2026-02-07 23:10:40 +0530

    Delete ExpressKart(web) directory

--- ExpressKart(web)/client/clear-storage.js
+++
@@ -1,4 +0,0 @@
-// Run this in browser console to clear localStorage and fix authentication issues
-localStorage.clear();
-console.log('localStorage cleared successfully!');
-window.location.reload();
